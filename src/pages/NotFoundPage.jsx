f012a2fdefa5fa3b2518f57c262cba6056e4eecf
KuznetsovaMaria <marysergkuz@gmail.com>
2025-04-05 22:06:36 +0500

    Style: fix errorPage layout and style

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router';
 export default function ErrorPage() {
   return (
     <>
-      <h1>404 This page doesn't exist</h1>
-      <div>
+      <div className="error-container">
+        <h1>404 This page doesn't exist</h1>
         <p>If you were looking for a contact form, try:</p>
         <Link to="/Contact-form/">Contact form</Link>
       </div>
